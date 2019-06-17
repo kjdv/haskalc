e2f8d6b34a3fc31a9ff38537b6bab2d14ecfc48f
Klaas de Vries <klaasjacobdevries@gmail.com>
2019-06-17 13:30:55 +0100

    complete? tokenizer

--- src/Tokenize.hs
+++ src/Tokenize.hs
@@ -1,13 +1,38 @@
 module Tokenize where
 
+import Data.Char
+
 data Token =
     Plus
   | Minus
   | Times
   | Divide
+  | Power
+  | Open
+  | Close
+  | Comma
+  | Identifier String
+  | Number Double
   deriving (Show, Eq)
 
 tokenize :: String -> [Token]
 tokenize [] = []
+
 tokenize ('+':xs) = Plus : tokenize xs
-tokenize (x:_) = error ("unrecognized token '" ++ [x] ++ "'")
+tokenize ('-':xs) = Minus : tokenize xs
+tokenize ('*':xs) = Times : tokenize xs
+tokenize ('/':xs) = Divide : tokenize xs
+tokenize ('^':xs) = Power : tokenize xs
+
+tokenize ('(':xs) = Open : tokenize xs
+tokenize (')':xs) = Close : tokenize xs
+tokenize (',':xs) = Comma : tokenize xs
+
+tokenize (x:xs)
+  | isSpace(x) = tokenize xs
+  | isAlpha(x) = let (alphas,rest) = break (not . isAlpha) xs in Identifier (x:alphas) : tokenize rest
+  | isDigit(x) = do
+      let results = reads (x:xs)
+      let (num,rest) = head results
+      Number num : tokenize rest
+  | otherwise  = error ("unrecognized token '" ++ [x] ++ "'")
--- test/TokenizeSpec.hs
+++ test/TokenizeSpec.hs
@@ -1,10 +1,30 @@
 module TokenizeSpec where
 
 import Test.Hspec
+import Control.Exception (evaluate)
 import Tokenize
 
 tokenizeSpec :: Spec
 tokenizeSpec = do
   describe "tokenize" $ do
-    it "recongnizes the + sign" $ do
-      tokenize "+" `shouldBe` [Plus]
+    it "recongnizes the math operators" $ do
+      tokenize "+-*/^" `shouldBe` [Plus,Minus,Times,Divide,Power]
+
+    it "recognizes parenthesis" $ do
+      tokenize "(,)" `shouldBe` [Open,Comma,Close]
+
+    it "recognizes identifiers" $ do
+      tokenize "a" `shouldBe` [Identifier "a"]
+      tokenize "abc" `shouldBe` [Identifier "abc"]
+
+    it "recognizes numbers" $ do
+      tokenize "1" `shouldBe` [Number 1.0]
+      tokenize "3.14" `shouldBe` [Number 3.14]
+      tokenize "1e2" `shouldBe` [Number 100.0]
+
+    it "splits tokens" $ do
+      tokenize "+ -" `shouldBe` [Plus,Minus]
+      tokenize "1abc2 def ^" `shouldBe` [Number 1.0, Identifier "abc", Number 2.0, Identifier "def", Power]
+
+    it "fails on bad tokens" $ do
+      evaluate (tokenize "£") `shouldThrow` errorCall "unrecognized token '£'"
