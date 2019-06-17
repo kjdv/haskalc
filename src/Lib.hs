bebb55f50cadaa6ae1ef810ee9c1ebd630487d33
Klaas de Vries <klaasjacobdevries@gmail.com>
2019-06-17 13:47:17 +0100

    adding readline-like support

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,19 @@ module Lib
     ( someFunc
     ) where
 
+import System.Console.Haskeline
+
+readEvalPrintLoop :: IO ()
+readEvalPrintLoop = runInputT defaultSettings loop
+  where
+    loop :: InputT IO ()
+    loop = do
+      minput <- getInputLine ">> "
+      case minput of
+        Nothing -> return ()
+        Just "quit" -> return ()
+        Just input -> do outputStrLn $ "Input was: " ++ input
+                         loop
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = readEvalPrintLoop
